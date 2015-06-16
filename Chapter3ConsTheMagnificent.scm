18fa3723bee7a9d8d7a40bbcb7a914ad3b1134cb
eewilliac <eewilliac@gmail.com>
2015-06-16 09:13:09 -0400

    git ignore and other code

--- Chapter3ConsTheMagnificent.scm
+++ Chapter3ConsTheMagnificent.scm
@@ -1,7 +1,18 @@
 #lang racket
 
+(define alonglist (list 'peppers 'and 'tomato 'and 'potato 'and 'lettuce 'and 'celery 'and 'kibble 'and 'bits 'and 'and 'codfish 'and 'ackee))
+
 (define atom?
   (lambda(expression)
     (cond
       ((not(null? expression))(not(pair? expression)))
       (else #f))))
+
+
+(define member?
+  (lambda(atom lat)
+    (cond
+      ((null? lat)#f)
+      ((or(eq? atom (car lat))(member? atom (cdr lat))))
+      (else #f))))
+       

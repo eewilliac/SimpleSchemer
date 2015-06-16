ce9636bf1ca0110dc5edb33c6c03d1338c488eff
eewilliac <eewilliac@gmail.com>
2015-06-16 13:16:02 -0400

    finished insertR function"

--- Chapter3ConsTheMagnificent.scm
+++ Chapter3ConsTheMagnificent.scm
@@ -1,6 +1,11 @@
 #lang racket
 
 (define alonglist (list 'peppers 'and 'tomato 'and 'potato 'and 'lettuce 'and 'celery 'and 'kibble 'and 'bits 'and 'and 'codfish 'and 'ackee))
+(define firstlist (list (list 'apple 'peach 'pumpkin)
+                        (list 'plum 'pear 'cherry)
+                        (list 'grape 'raisin 'pea)
+                        (list 'bean 'carrot 'eggplant)))
+                             
 
 (define atom?
   (lambda(expression)
@@ -16,3 +21,29 @@
       ((or(eq? atom (car lat))(member? atom (cdr lat))))
       (else #f))))
        
+(define rember
+  (lambda(atom lat)
+    (cond
+      ((null? lat)(quote()))
+      ((eq? atom (car lat))(cdr lat))
+      (else
+       (cons (car lat)(rember atom (cdr lat)))))))
+
+
+(define firsts
+  (lambda(lat)
+    (cond
+      ((null? lat)(quote()))
+      (else
+       (cons (car (car lat))(firsts (cdr lat)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat)(quote()))
+      ((eq? (car lat) old)(cons old(cons new (cdr lat))))
+      (else
+       (cons (car lat)(insertR new old (cdr lat)))))))
+
+      
+     

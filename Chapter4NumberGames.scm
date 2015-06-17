27ce72ed9e697537cf62a4704fd2fe8aaea868fd
eewilliac <eewilliac@gmail.com>
2015-06-17 10:46:18 -0400

    need to figure out tuples

---
+++ Chapter4NumberGames.scm
@@ -0,0 +1,29 @@
+#lang racket
+(define sub1
+  (lambda(num)
+    (- num 1)))
+
+ (define add1
+   (lambda(num)
+     (+ num 1)))
+
+(define recursiveSubtract
+  (lambda (a b)
+    (cond
+      ((zero? b)a)
+      (else
+       (sub1(recursiveSubtract a(sub1 b)))))))
+
+(define recursiveAdd
+  (lambda(a b)
+    (cond
+      ((zero? b)a)
+      (else
+       (add1 (recursiveAdd a (sub1 b)))))))
+
+(define tupadd
+  (lambda(atuple)
+    (cond
+      ((null atuple)0)
+      (else
+       (recursiveAdd (car atuple)(tupadd (cdr atuple)))))))

76ba7df6ae180757861ccec9db4d3b5d7f13e49c
eewilliac <eewilliac@gmail.com>
2015-06-21 08:42:49 -0400

    SimpleLists

---
+++ SimpleLists.scm
@@ -0,0 +1,10 @@
+#lang racket
+
+(define simple1 (list (list (list 'hotdogs)) (list 'and) (list 'pickle) 'relish))
+(define simple2 (list (list (list 'hotdogs)) (list 'and)))
+
+(define alist '())
+(define simpleList (list 'elem1 'elem2))
+(define alist2 '(x y)) ;this is the new way I should make lists.
+
+(define simple '(((hotdog))(and)(pickle))) ;this is the new way I should make lists.
